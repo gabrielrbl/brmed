23a90325e2637c1b475b32916f602222a99a1854
Gabriel Lobo <gabrielrblgbi@gmail.com>
2022-10-07 03:51:42 -0300

    fix: up to date static files storage

--- brmed/settings.py
+++ brmed/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
