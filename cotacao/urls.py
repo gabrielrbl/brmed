6caaab0ac0aa0e33f5309d06dc3b761df73f9780
Gabriel Lobo <gabrielrblgbi@gmail.com>
2022-10-07 02:41:37 -0300

    feat: register viewsets in a DefaultRouter

--- cotacao/urls.py
+++ cotacao/urls.py
@@ -1,6 +1,13 @@
+from rest_framework.routers import DefaultRouter
 from django.urls import path
+from django.conf.urls import include
 from . import views
+
+router = DefaultRouter(trailing_slash=False)
+router.register(r'currencies', views.CurrencyViewSet, basename='currencies')
+router.register(r'rates', views.RateViewSet, basename='rates')
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('api/', include(router.urls)),
 ]
